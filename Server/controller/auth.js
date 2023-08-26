352de7bbc4038bfcc4f31811751c0c80e41a35f8
vetri <vetri@Lenovo-V15-G2-ALC-Ua-3f36816e.localdomain>
2023-08-26 14:42:23 +0530

    main branch merged with client branch

--- Server/controller/auth.js
+++ Server/controller/auth.js
@@ -22,7 +22,7 @@ export const register = async (req, res, next) => {
 
     const newUser = new User({
       username: req.body.username,
-      email: req.body.email,
+      email: req.body.email, 
       password: hash,
     });
     // Saving new User
@@ -70,5 +70,5 @@ export const login = async (req, res, next) => {
       .json({ message: "Logged In SuccessFully!!!", user: user, token });
   } catch (err) {
     next(err);
-  }
+  } 
 };
