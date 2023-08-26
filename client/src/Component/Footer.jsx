d08520d27717ca933384e0c2353d3d291cfacdd5
vetri <vetri@Lenovo-V15-G2-ALC-Ua-3f36816e.localdomain>
2023-08-26 10:38:42 +0530

    basic template of client -vetrivel

---
+++ client/src/Component/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+
+const Footer = () =>{
+    return(
+        <>
+        <div className="footerContainer">
+
+            <footer>
+                footer 
+            </footer>
+        </div>
+        </>
+    );
+};
+
+
+export default Footer;
---
+++ client/src/Component/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+
+
+const Home = () => {
+    return(
+        <>
+        <Navbar/>
+        <div className="container">
+            Home Page
+        </div>
+        <Footer/>
+        </>
+    )
+}
+
+export default Home;
