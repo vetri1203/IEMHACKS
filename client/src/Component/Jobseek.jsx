bb2cc007378022f168019e7ae55fb8701112a10c
vetri <vetri@Lenovo-V15-G2-ALC-Ua-3f36816e.localdomain>
2023-08-27 15:28:08 +0530

    client side search Completed without style

--- client/src/Component/Jobseek.jsx
+++ client/src/Component/Jobseek.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
+import './Style/Search.css'
+import Nav2 from "./Navbar2";
+const JobSeek = () => {
+    const [JobName, setJobName] = useState('');
+    const [matchingPosts, setMatchingPosts] = useState([]);
 
-const Jonseek = () =>{
-    return(
-        <>
+    const SearchJob = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post("http://localhost:8081/search", {
+                JobName,
+            });
+
+            if (response.data.matchingPosts != null) {
+                setMatchingPosts(response.data.matchingPosts);
+                console.log(response.data.matchingPosts);
+            }
+        } catch (error) {
+            console.error("Error searching for jobs:", error);
+        }
+    }
+
+    return (
         <div className="containerJob">
+            <Nav2/>
+            <form onSubmit={SearchJob}>
+                <input type="text" value={JobName} onChange={e => setJobName(e.target.value)} placeholder="Enter the Skills" />
+                <button type="submit">Search</button>
+            </form>
             
+            <div className="output">
+                {matchingPosts.map((data, index) => (
+                    <div key={index} id={index} className='common ${index}'>
+                        {/* Display your data details here */}
+                        <p> <span className="heading">JobName:</span><span>{data.JobName}</span> </p>
+                        <p><span className="heading">Skills:</span> <span>{data.Skills}</span></p>
+                        <p><span className="heading">AboutCompany:</span> <span>{data.AboutCompany}</span></p>
+                        <br />
+                        {/* Add more properties as needed */}
+                    </div>
+                ))}
+            </div>
         </div>
-        </>
-    )
+    );
 }
 
-export default Jonseek;
+export default JobSeek;
